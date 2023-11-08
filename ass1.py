43f26e2fcccd9a443d954abc5161d52c18ae78df
Vitur <vitur2000@hotmail.com>
2023-11-08 18:03:15 -0500

    Assignment 2 complete by ALL of US

--- ass1.py
+++ ass1.py
@@ -1,152 +1,189 @@
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score, accuracy_score
-from sklearn.model_selection import GridSearchCV
-import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+
+def save_confusion_matrix_plot(conf_matrix, title, filename):
+    plt.figure(figsize=(6, 4))
+    plt.imshow(conf_matrix, interpolation='nearest', cmap=plt.cm.Blues)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(conf_matrix))
+    plt.xticks(tick_marks, tick_marks)
+    plt.yticks(tick_marks, tick_marks)
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.tight_layout()
+    plt.savefig(filename)
 
 
 
-# Method 1: Convert the categorical variables into one-hot encoded vectors
-def method1_hotVector(datafram,columns):
-    return pd.get_dummies(datafram, columns)
+penguin_data = pd.read_csv('penguins.csv')
+abalone_data = pd.read_csv('abalone.csv')
+
+print(abalone_data.head())
+print(abalone_data.columns)
+
+penguin_data = pd.get_dummies(penguin_data, columns=['island', 'sex'])
+
+penguin_class_distribution = penguin_data['species'].value_counts()
+penguin_class_distribution.plot(kind='bar', title='Penguin Class Distribution')
+plt.savefig('penguin-classes.png')
+
+abalone_class_distribution = abalone_data['Type'].value_counts()
+abalone_class_distribution.plot(kind='bar', title='Abalone Class Distribution')
+plt.savefig('abalone-classes.png')
+
+penguin_X = penguin_data.drop('species', axis=1)
+penguin_y = penguin_data['species']
+abalone_X = abalone_data.drop('Type', axis=1)
+abalone_y = abalone_data['Type']
+
+penguin_X_train, penguin_X_test, penguin_y_train, penguin_y_test = train_test_split(penguin_X, penguin_y, test_size=0.2,
+                                                                                    random_state=42)
+abalone_X_train, abalone_X_test, abalone_y_train, abalone_y_test = train_test_split(abalone_X, abalone_y, test_size=0.2,
+                                                                                    random_state=42)
 
 
-# Method 2: Convert to categories manually
-def method2_manual(dataframe, column):
-    dataframe[column] = dataframe[column].astype('category')
-    return dataframe[column].cat.codes
+penguin_base_dt = DecisionTreeClassifier()
+penguin_base_dt.fit(penguin_X_train, penguin_y_train)
+plot_tree(penguin_base_dt, filled=True, rounded=True, feature_names=penguin_X.columns, class_names=penguin_y.unique())
+plt.savefig('penguin-base-dt.png')
 
-def train_and_test_base_decision_tree(X_train, X_test, y_train, y_test, dataset_name):
-    # Create and train the base Decision Tree model
-    base_dt_model = DecisionTreeClassifier()
-    base_dt_model.fit(X_train, y_train)
-
-    # Test the model and collect performance metrics
-    predictions = base_dt_model.predict(X_test)
-
-    # Calculate performance metrics
-    confusion = confusion_matrix(y_test, predictions)
-    precision = precision_score(y_test, predictions, average=None)
-    recall = recall_score(y_test, predictions, average=None)
-    f1 = f1_score(y_test, predictions, average=None)
-    accuracy = accuracy_score(y_test, predictions)
-
-    # Visualize the decision tree (optional)
-    # You can use graphviz or other libraries for tree visualization
+abalone_base_dt = DecisionTreeClassifier(max_depth=3)
+abalone_base_dt.fit(abalone_X_train, abalone_y_train)
+plot_tree(abalone_base_dt, filled=True, rounded=True, feature_names=abalone_X.columns, class_names=abalone_y.unique())
+plt.savefig('abalone-base-dt.png')
 
 
-def train_and_test_top_decision_tree(X_train, X_test, y_train, y_test, dataset_name):
-    # Create and train the top Decision Tree model using grid search
-    # Experiment with hyperparameters (criterion, max depth, min samples split)
-    from sklearn.model_selection import GridSearchCV
+param_grid = {
+    'criterion': ['gini', 'entropy'],
+    'max_depth': [None, 5, 10],
+    'min_samples_split': [2, 5, 10]
+}
+grid_search_penguin_dt = GridSearchCV(DecisionTreeClassifier(), param_grid, scoring='accuracy', cv=5)
+grid_search_penguin_dt.fit(penguin_X_train, penguin_y_train)
+best_penguin_dt = grid_search_penguin_dt.best_estimator_
+plot_tree(best_penguin_dt, filled=True, rounded=True, feature_names=penguin_X.columns, class_names=penguin_y.unique())
+plt.savefig('penguin-top-dt.png')
 
-    param_grid = {
-        'criterion': ['gini', 'entropy'],
-        'max_depth': [None, 5, 10],  # Adjust these values as needed
-        'min_samples_split': [2, 5, 10]  # Adjust these values as needed
-    }
-
-    grid_search = GridSearchCV(DecisionTreeClassifier(), param_grid, scoring='f1_macro')
-    grid_search.fit(X_train, y_train)
-
-    top_dt_model = grid_search.best_estimator_
-
-    # Test the model and collect performance metrics
-    predictions = top_dt_model.predict(X_test)
-
-    # Calculate performance metrics
-    confusion = confusion_matrix(y_test, predictions)
-    precision = precision_score(y_test, predictions, average=None)
-    recall = recall_score(y_test, predictions, average=None)
-    f1 = f1_score(y_test, predictions, average=None)
-    accuracy = accuracy_score(y_test, predictions)
-
-    # Visualize the decision tree (optional)
-    # You can use graphviz or other libraries for tree visualization
+grid_search_abalone_dt = GridSearchCV(DecisionTreeClassifier(), param_grid, scoring='accuracy', cv=5)
+grid_search_abalone_dt.fit(abalone_X_train, abalone_y_train)
+best_abalone_dt = grid_search_abalone_dt.best_estimator_
+plot_tree(best_abalone_dt, filled=True, rounded=True, feature_names=abalone_X.columns, class_names=abalone_y.unique())
+plt.savefig('abalone-top-dt.png')
 
 
-def train_and_test_base_mlp(X_train, X_test, y_train, y_test, dataset_name):
-    # Create and train the base Multi-Layered Perceptron (MLP) model
-    base_mlp_model = MLPClassifier(hidden_layer_sizes=(100, 100), activation='logistic', solver='sgd')
-    base_mlp_model.fit(X_train, y_train)
+penguin_base_mlp = MLPClassifier(hidden_layer_sizes=(100, 100), activation='logistic', solver='sgd', random_state=42)
+penguin_base_mlp.fit(penguin_X_train, penguin_y_train)
 
-    # Test the model and collect performance metrics
-    predictions = base_mlp_model.predict(X_test)
-
-    # Calculate performance metrics
-    confusion = confusion_matrix(y_test, predictions)
-    precision = precision_score(y_test, predictions, average=None)
-    recall = recall_score(y_test, predictions, average=None)
-    f1 = f1_score(y_test, predictions, average=None)
-    accuracy = accuracy_score(y_test, predictions)
+abalone_base_mlp = MLPClassifier(hidden_layer_sizes=(100, 100), activation='logistic', solver='sgd', random_state=42)
+abalone_base_mlp.fit(abalone_X_train, abalone_y_train)
 
 
-def train_and_test_top_mlp(X_train, X_test, y_train, y_test, dataset_name):
-    # Create and train the top MLP model using grid search
-    # Experiment with hyperparameters (activation function, network architecture, solver)
+param_grid_mlp = {
+    'activation': ['logistic', 'tanh', 'relu'],
+    'hidden_layer_sizes': [(30, 50), (10, 10, 10)],
+    'solver': ['adam', 'sgd']
+}
+grid_search_penguin_mlp = GridSearchCV(MLPClassifier(), param_grid_mlp, scoring='accuracy', cv=5)
+grid_search_penguin_mlp.fit(penguin_X_train, penguin_y_train)
+best_penguin_mlp = grid_search_penguin_mlp.best_estimator_
 
-    param_grid = {
-        'activation': ['logistic', 'tanh', 'relu'],
-        'hidden_layer_sizes': [(30, 50), (10, 10, 10)],  # Adjust these architectures
-        'solver': ['adam', 'sgd']
-    }
-
-    grid_search = GridSearchCV(MLPClassifier(), param_grid, scoring='f1_macro')
-    grid_search.fit(X_train, y_train)
-
-    top_mlp_model = grid_search.best_estimator_
-
-    # Test the model and collect performance metrics
-    predictions = top_mlp_model.predict(X_test)
-
-    # Calculate performance metrics
-    confusion = confusion_matrix(y_test, predictions)
-    precision = precision_score(y_test, predictions, average=None)
-    recall = recall_score(y_test, predictions, average=None)
-    f1 = f1_score(y_test, predictions, average=None)
-    accuracy = accuracy_score(y_test, predictions)
+grid_search_abalone_mlp = GridSearchCV(MLPClassifier(), param_grid_mlp, scoring='accuracy', cv=5)
+grid_search_abalone_mlp.fit(abalone_X_train, abalone_y_train)
+best_abalone_mlp = grid_search_abalone_mlp.best_estimator_
 
 
-if __name__ == "__main__":
-    # Load the penguins dataset
-    df_penguins = pd.read_csv('penguins.csv')
 
-    # Display the first few rows of the dataset
-    print(df_penguins.head())
+def save_model_performance(model, X_test, y_test, dataset_name, model_name, hyperparameters, output_file):
+    y_pred = model.predict(X_test)
+    conf_matrix = confusion_matrix(y_test, y_pred)
+    save_confusion_matrix_plot(conf_matrix, f'{dataset_name} - {model_name}',
+                               f'{dataset_name.lower()}-{model_name}.png')
+    classification_rep = classification_report(y_test, y_pred, target_names=y_test.unique(), output_dict=True)
+    accuracy = accuracy_score(y_test, y_pred)
+    macro_f1 = classification_rep['macro avg']['f1-score']
+    weighted_f1 = classification_rep['weighted avg']['f1-score']
 
-    columns =['species', 'sex', 'island']
-
-    # Method 1: Convert these features into 1-hot vectors
-    df_penguins_encoder = method1_hotVector(df_penguins,columns)
-
-    # Method 2: Convert to categories manually
-    for column in columns:
-        df_penguins[column] = method2_manual(df_penguins, column)
-
-    # Load the penguins dataset
-    df_abalone = pd.read_csv('abalone.csv')
-
-    # Display the first few rows of the dataset
-    print(df_abalone.head())
-
-
-    # Method 1: Convert these features into 1-hot vectors
-    df_abalone_encoder = method1_hotVector(df_abalone,'Type')
-
-    # Method 2: Convert to categories manually
-    df_abalone['Type'] = method2_manual(df_abalone, 'Type')
+    with open(output_file, 'a') as f:
+        f.write(f'{model_name} - {hyperparameters}\n')
+        f.write('Confusion Matrix:\n')
+        f.write(str(conf_matrix) + '\n')
+        f.write('Classification Report:\n')
+        f.write(str(classification_rep) + '\n')
+        f.write(f'Accuracy: {accuracy}\n')
+        f.write(f'Macro-average F1: {macro_f1}\n')
+        f.write(f'Weighted-average F1: {weighted_f1}\n')
+        f.write('-' * 40 + '\n')
 
 
-    # Display the first few rows to verify the changes
-    print("Method 1:")
-    print(df_penguins_encoder.head())
-    print("\nMethod 2:")
-    print(df_penguins.head())
 
-    # Display the first few rows to verify the changes
-    print("Method 1:")
-    print(df_abalone_encoder.head())
-    print("\nMethod 2:")
-    print(df_abalone.head())
+penguin_performance_file = 'penguin-performance.txt'
+abalone_performance_file = 'abalone-performance.txt'
 
+
+for i in range(5):
+    save_model_performance(penguin_base_dt, penguin_X_test, penguin_y_test, 'Penguin', 'Base-DT', 'Default',
+                           penguin_performance_file)
+    save_model_performance(best_penguin_dt, penguin_X_test, penguin_y_test, 'Penguin', 'Top-DT',
+                           grid_search_penguin_dt.best_params_, penguin_performance_file)
+    save_model_performance(penguin_base_mlp, penguin_X_test, penguin_y_test, 'Penguin', 'Base-MLP', 'Default',
+                           penguin_performance_file)
+    save_model_performance(best_penguin_mlp, penguin_X_test, penguin_y_test, 'Penguin', 'Top-MLP',
+                           grid_search_penguin_mlp.best_params_, penguin_performance_file)
+
+    save_model_performance(abalone_base_dt, abalone_X_test, abalone_y_test, 'Abalone', 'Base-DT', 'Default',
+                           abalone_performance_file)
+    save_model_performance(best_abalone_dt, abalone_X_test, abalone_y_test, 'Abalone', 'Top-DT',
+                           grid_search_abalone_dt.best_params_, abalone_performance_file)
+    save_model_performance(abalone_base_mlp, abalone_X_test, abalone_y_test, 'Abalone', 'Base-MLP', 'Default',
+                           abalone_performance_file)
+    save_model_performance(best_abalone_mlp, abalone_X_test, abalone_y_test, 'Abalone', 'Top-MLP',
+                           grid_search_abalone_mlp.best_params_, abalone_performance_file)
+
+
+
+def calculate_average_performance(dataset_name, model_name, num_iterations, performance_file):
+    accuracy_list = []
+    macro_f1_list = []
+    weighted_f1_list = []
+
+    with open(performance_file, 'r') as f:
+        lines = f.read().splitlines()
+        for i in range(num_iterations):
+            model_info = f'{model_name} - Default' if i == 0 else f'{model_name} - {lines[i * 11 + 1][len(model_name) + 2:]}'
+            accuracy = float(lines[i * 11 + 5][len('Accuracy: '):])
+            macro_f1 = float(lines[i * 11 + 6][len('Macro-average F1: '):])
+            weighted_f1 = float(lines[i * 11 + 7][len('Weighted-average F1: '):])
+
+            accuracy_list.append(accuracy)
+            macro_f1_list.append(macro_f1)
+            weighted_f1_list.append(weighted_f1)
+
+    avg_accuracy = np.mean(accuracy_list)
+    avg_macro_f1 = np.mean(macro_f1_list)
+    avg_weighted_f1 = np.mean(weighted_f1_list)
+
+    accuracy_var = np.var(accuracy_list)
+    macro_f1_var = np.var(macro_f1_list)
+    weighted_f1_var = np.var(weighted_f1_list)
+
+    with open(performance_file, 'a') as f:
+        f.write(f'Average Accuracy: {avg_accuracy} (Variance: {accuracy_var})\n')
+        f.write(f'Average Macro-average F1: {avg_macro_f1} (Variance: {macro_f1_var})\n')
+        f.write(f'Average Weighted-average F1: {avg_weighted_f1} (Variance: {weighted_f1_var})\n')
+
+
+calculate_average_performance('Penguin', 'Base-DT', 5, penguin_performance_file)
+calculate_average_performance('Penguin', 'Top-DT', 5, penguin_performance_file)
+calculate_average_performance('Penguin', 'Base-MLP', 5, penguin_performance_file)
+calculate_average_performance('Penguin', 'Top-MLP', 5, penguin_performance_file)
+
+calculate_average_performance('Abalone', 'Base-DT', 5, abalone_performance_file)
+calculate_average_performance('Abalone', 'Top-DT', 5, abalone_performance_file)
+calculate_average_performance('Abalone', 'Base-MLP', 5, abalone_performance_file)
+calculate_average_performance('Abalone', 'Top-MLP', 5, abalone_performance_file)
